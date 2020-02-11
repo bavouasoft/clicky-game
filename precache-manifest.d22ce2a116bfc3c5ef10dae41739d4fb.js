942776fbeff097462422c4f747695980d10c0e9f
Achille Bavoua <bavouasoft@yahoo.fr>
2020-02-10 21:02:12 -0500

    Updates

---
+++ precache-manifest.d22ce2a116bfc3c5ef10dae41739d4fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69341de821e4cff046a39dd36538bd40",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c165b140a7343ec83294",
+    "url": "/static/css/main.7eedff02.chunk.css"
+  },
+  {
+    "revision": "646a75c58be7259d17ba",
+    "url": "/static/js/2.8c5b1c2e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.8c5b1c2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c165b140a7343ec83294",
+    "url": "/static/js/main.1611529c.chunk.js"
+  },
+  {
+    "revision": "cc2927c6818dc6c4f663",
+    "url": "/static/js/runtime-main.f75059d1.js"
+  }
+]);
